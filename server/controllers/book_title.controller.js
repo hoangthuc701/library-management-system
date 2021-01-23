55ac2cd4d72b2bc04b8ad10114dfd1178fb9c87b
NguyenMinhTam-max <nguyenminhtam1106999@gmail.com>
2021-01-23 21:18:32 +0700

    add new db script, add sample controller

--- server/controllers/book_title.controller.js
+++ server/controllers/book_title.controller.js
@@ -10,7 +10,15 @@ module.exports = {
 	
         var listBookTitle = await BookTitle.loadByOffset((p - 1) * 10);
         var quantity = await BookTitle.quantity();
-		console.log(listBookTitle);
 		res.json({list: listBookTitle, quantity: quantity, rangeOfPages:functUtils.rangeOfPagination(Math.ceil(quantity[0]["quantity"] / 10), p)});
+	},
+	getByID: async(req,res) => {
+		var id = req.params.id;
+		res.json(await BookTitle.loadByID(id))
+	},
+	delete: async(req,res) => {
+		var id = req.params.id;
+		await BookTitle.delete(id);
+		res.json(true);
 	}
 };
--- server/routes/admin.route.js
+++ server/routes/admin.route.js
@@ -6,6 +6,9 @@ const bookTitleController = require('../controllers/book_title.controller');
 router.get('/admin/Books', bookController.getByOffset);
 
 router.get('/admin/BookTitles', bookTitleController.getByOffset);
+router.get('/admin/BookTitles/:id', bookTitleController.getByID);
+router.get('/admin/BookTitles/del/:id', bookTitleController.delete);
+
 
 router.get('/admin/Accounts', accountController.getByOffset);
 router.get('/admin/Accounts/add',accountController.add)
